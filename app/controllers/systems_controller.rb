02a83c0d3e40b8632c98a67b42df00176ea8e6fa
barcelosrui <barcelos.rui@gmail.com>
2017-11-06 00:08:13 +0000

    Add update and Destroy System
    add try catch if url parameter in edit or show not exists

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -1,18 +1,45 @@
 class SystemsController < ApplicationController
     def new
+        @system = System.new
     end
     def index
         @systems = System.all
       end
     def show
         @system = System.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        redirect_to(systems_path, :notice => 'Record not found')
     end
     
     def create
         @system = System.new(system_params)
-   
-        @system.save
-        redirect_to @system
+        
+        if @system.save
+            redirect_to @system
+          else
+            render 'new'
+          end
+    end
+    def edit
+        @system = System.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+            redirect_to(systems_path, :notice => 'Record not found')
+      end
+    def update
+        @system = System.find(params[:id])
+       
+        if @system.update(system_params)
+          redirect_to @system
+        else
+          render 'edit'
+        end
+      end
+    
+    def destroy
+        @system = System.find(params[:id])
+        @system.destroy
+    
+        redirect_to systems_path
     end
    
   private
--- app/models/system.rb
+++ app/models/system.rb
@@ -1,2 +1,4 @@
 class System < ApplicationRecord
+    validates :name, presence: true,
+    length: { minimum: 3 }
 end
