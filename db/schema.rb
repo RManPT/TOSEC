176c7fec9226634c260301ffd05e50bf78749b52
Rui Rodrigues <rman2006@hotmail.com>
2017-11-10 06:28:28 +0000

    Merge branch 'teste' into Releases

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107192435) do
+ActiveRecord::Schema.define(version: 20171107143031) do
 
   create_table "collection_types", force: :cascade do |t|
     t.string "name"
@@ -43,12 +43,6 @@ ActiveRecord::Schema.define(version: 20171107192435) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "dateRelease"
-  end
-
-  create_table "systems_types", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
