5e3d00af7c9c69aed844b8bb08ab0db99018d52e
Rui Rodrigues <rman2006@hotmail.com>
2017-11-11 20:54:06 +0000

    Added dat files controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,26 @@
 
 //= require moment
 //= require bootstrap-datetimepicker
+
+//start - Back top top jquery
+$(document).ready(function(){
+    $(window).scroll(function () {
+           if ($(this).scrollTop() > 50) {
+               $('#back-to-top').fadeIn();
+           } else {
+               $('#back-to-top').fadeOut();
+           }
+       });
+       // scroll body to 0px on click
+       $('#back-to-top').click(function () {
+           $('#back-to-top').tooltip('hide');
+           $('body,html').animate({
+               scrollTop: 0
+           }, 800);
+           return false;
+       });
+       
+       $('#back-to-top').tooltip('show');
+
+});
+//end - Back top top jquery
