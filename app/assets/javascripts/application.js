a7b41b1ae63349abf97f7b082ce98ec23ee92df3
Rui Rodrigues <rman2006@hotmail.com>
2017-11-19 00:47:18 +0000

    Solved issue with datepicker only working on hard reload
    That happened because of turbolinks. Document.ready does not work. 
    
    Use $(document).on('turbolinks:load', function () {}

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
 
 //= require jquery
+//= require turbolinks
+
 //= require bootstrap-sprockets
 
 //= require moment
