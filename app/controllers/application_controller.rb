856ba1f1fbe7efe3b08228da9da885c7e5196116
David <david_carrilho92@hotmail.com>
2017-12-03 23:18:32 +0000

    Users Other Changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
-    before_action :init
+  before_action :init
+
+  # load_and_authorize_resource
+  # rescue_from CanCan::AccessDenied do |exception|
+  #   redirect_to '/', :alert => exception.message
+  # end
     
     def init
       @controller = controller_name
