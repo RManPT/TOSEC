eb65babb572620e420d2b1da7962de839089c269
David <david_carrilho92@hotmail.com>
2017-12-03 18:22:10 +0000

    Users CRUD + Admin Attribute

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #devise_for :admins
   devise_for :users
+  resources :users
   
   get 'welcome/start'
   get 'welcome/whatisit'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127221810) do
+ActiveRecord::Schema.define(version: 20171203170603) do
 
   create_table "collection_types", force: :cascade do |t|
     t.string "name"
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 20171127221810) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
