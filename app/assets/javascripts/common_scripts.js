a7ec9dbe646ff4110d1728a3eaebd47a330b44c1
Rui Rodrigues <rman2006@hotmail.com>
2017-12-18 09:26:43 +0000

    Added tooltip, td onclick and dynamic links scripts
    - Costumizable tooltips
    - Added onclick event to <td data-link> so we can click to see details when listing items
    - Added dynamic home button when not in welcome#start
    - Added dynamic back button when history is not empty
    - Added dynamic top button when scroll e greater than 50

--- app/assets/javascripts/common_scripts.js
+++ app/assets/javascripts/common_scripts.js
@@ -2,10 +2,20 @@
 
 
 //START - jquery Back to top snippet 
-$(document).ready(function () {
+$(document).on('turbolinks:load', function () {
     $(window).scroll(function () {
         if ($(this).scrollTop() > 50) {
-            $('#back-to-top').fadeIn();
+            $('#back-to-top').fadeIn();   
+            if (history.length > 2 || document.referrer.length > 0){
+                $('#back-to-back').fadeIn();  
+            }    
+            var a = $('#invisible1').data('action');
+            var c = $('#invisible2').data('controler');
+            if (c != "welcome" && a != "start"){
+                $('#back-to-home').fadeIn();  
+            } else {
+                $('#back-to-home').fadeOut(); 
+            }
         } else {
             $('#back-to-top').fadeOut();
         }
@@ -18,13 +28,101 @@ $(document).ready(function () {
         }, 800);
         return false;
     });
-    $('#back-to-top').tooltip('show');
+    $(function() {
+        if (history.length > 1 || document.referrer.length > 0){
+            $('#back-to-back').fadeIn();  
+        } else {
+            $('#back-to-back').fadeOut(); 
+        }
+    });
+    $(function() {
+        var a = $('#invisible1').data('action');
+        var c = $('#invisible2').data('controler');
+        if (c != "welcome" && a != "start"){
+            $('#back-to-home').fadeIn();  
+        } else {
+            $('#back-to-home').fadeOut(); 
+        }
+    });    
 });
 //END - jquery Back to top snippet 
 
+//START - jquery Back to back snippet 
+
+//END - jquery Back to back snippet 
+
+
 //START - jquery Enables tooltips 
-$(document).on('turbolinks:load', function(){
-    $('[data-toggle="tooltip"]').tooltip();
+$(document).on('turbolinks:load', function () {
+    $('[data-toggle="tooltip"]').tooltip({container: "body"}); 
 });
 //END - jquery Enables tooltips 
 
+
+
+
+
+$(document).on('turbolinks:load', function () {
+    $('td[data-toggle="tooltip"]').tooltip({
+        container: "body"
+    });
+})
+
+//START - jquery Enables clickable objects whilst allowing click events on "stop prop" class elements
+$(document).on('turbolinks:load', function () {
+    $("tr[data-link]").click(function (e) {
+        window.location = $(this).data("link");
+    });
+});
+
+$(document).on('turbolinks:load', function () {
+    $("div[data-link]").click(function (e) {
+        window.location = $(this).data("link");
+    });
+});
+
+$(document).on('turbolinks:load', function () {
+    $("td[data-link]").click(function (e) {
+        window.location = $(this).data("link");
+ 
+    });
+});
+
+$(document).on('turbolinks:load', function () {
+    $(".stop-prop").each(function () {
+        $(this).click(function (e) {
+            e.stopPropagation();
+        });
+    });
+});
+
+$(document).on('turbolinks:load', function () {
+        $('.delete[data-method="delete"]').click(function (e) {
+              e.preventDefault();
+            //  e.stopPropagation();
+            // confirm("Are you sure you want to delete?"); 
+        });
+    });
+
+// function show_confirm(url) {
+//     if (confirm("Are you sure you want to delete?"){
+//         $.ajax({
+//             url: url,
+//             type: 'DELETE',
+//             success: function(data) {
+//               //play with data
+//             }
+//           });
+//     }
+// }
+
+// $(document).on('turbolinks:load', function () {
+//     $(".delete").each(function () {
+//         $(this).click(function (e) {
+//             e.stopPropagation();
+// //            show_confirm($(this).attr("href"));
+//         });
+//     });
+// });
+
+//END - jquery Enables clickable table row
