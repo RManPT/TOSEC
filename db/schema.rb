172c846b186c39f7419ea8a1d18fea1cc462d0d9
Rui Barcelos <barcelos.rui@gmail.com>
2018-01-29 16:58:36 +0000

    UPDATE SCHEMA

--- db/schema.rb
+++ db/schema.rb
@@ -12,40 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20171231025012) do
 
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
   create_table "collection_types", force: :cascade do |t|
     t.string "name"
     t.text "desc"
@@ -87,14 +53,6 @@ ActiveRecord::Schema.define(version: 20171231025012) do
     t.integer "datfile_id", null: false
     t.integer "release_id", null: false
     t.index ["release_id", "datfile_id"], name: "index_datfiles_releases_on_release_id_and_datfile_id"
-  end
-
-  create_table "datfiles_status", force: :cascade do |t|
-    t.string "name"
-    t.string "abbreviation"
-    t.text "descState"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "datstatuses", force: :cascade do |t|
