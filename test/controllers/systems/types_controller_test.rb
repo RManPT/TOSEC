1a66d28cf81111717b4ac8ba051b00cf62bf8367
Rui Barcelos <barcelos.rui@gmail.com>
2018-02-02 23:55:07 +0000

    Teste Controller Systems::Type

--- test/controllers/systems/types_controller_test.rb
+++ test/controllers/systems/types_controller_test.rb
@@ -1,23 +1,28 @@
 require 'test_helper'
 
 class Systems::TypesControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @type = systems_types(:one)
+  end
+
   test "should get index" do
-    get systems_types_index_url
+    get systems_types_url
     assert_response :success
   end
 
   test "should get new" do
-    get systems_types_new_url
+    get new_systems_type_url
     assert_response :success
   end
 
   test "should get show" do
-    get systems_types_show_url
+    get systems_type_url(@type)
     assert_response :success
   end
 
   test "should get edit" do
-    get systems_types_edit_url
+    get edit_systems_type_url(@type)
     assert_response :success
   end
 
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get welcome_index_url
+  test "should get start" do
+    get welcome_start_url
     assert_response :success
   end
 
